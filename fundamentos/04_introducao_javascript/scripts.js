0b0f99a4b95e5d535fe296fc9fd514d403220cb5
Pan <pantojaosm@fastmail.com>
2022-05-24 10:21:36 -0400

    Ensino de javascript.

---
+++ fundamentos/04_introducao_javascript/scripts.js
@@ -0,0 +1,19 @@
+// Primeiros Passos - Variáveis
+
+var myName = "Osmar";
+const birthCity = "Manaus";
+let birthYear = 1997;
+
+// console.log (birthCity); Impressão no console.
+
+// const não pode retribuir valores. Somente var e let.
+
+// camelCase: Curvatura de camelo. Primeira letra minúscula. Segunda palavra maiúscula.
+
+// Tipos Primitivos
+
+let movie = "Vingadores"; // string literal.
+let score = 10.89; // number literal.
+let isValid = true; // boolean.
+let name; // undefined.
+let color = null; // redefinição.
